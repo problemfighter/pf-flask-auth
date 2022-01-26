319ef34537dce711951dd26d3a312aa3262603ee
hmtmcse <hmtm.cse@gmail.com>
2022-01-26 13:51:33 +0600

    CLI create Reset password

--- pf_flask_auth/cli/pf_flask_auth_cli.py
+++ pf_flask_auth/cli/pf_flask_auth_cli.py
@@ -1,6 +1,5 @@
 import click
 from flask.cli import AppGroup, with_appcontext
-
 from pf_flask_auth.service.operator_cli_service import operator_cli_service
 
 pf_flask_auth_cli = AppGroup("auth-cli", help="PF Flask Auth Manipulation")
