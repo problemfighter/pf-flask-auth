5bbae20b6907b9b07afaffeba4374867ab8be5a4
hmtmcse <hmtm.cse@gmail.com>
2023-03-04 15:18:33 +0600

    added validation methods

--- pf_flask_auth/common/pffa_auth_util.py
+++ pf_flask_auth/common/pffa_auth_util.py
@@ -1,4 +1,5 @@
 from pf_flask_auth.common.pffa_jwt_helper import jwt_helper
+from pf_flask_auth.data.pffa_form_auth_data import FormAuthData
 from pf_flask_rest_com.pf_flask_request_helper import request_helper
 
 
@@ -8,3 +9,7 @@ class AuthUtil:
     def get_api_auth_data():
         bearer_token = request_helper.get_bearer_token()
         return jwt_helper.validate_token(bearer_token)
+
+    @staticmethod
+    def get_ssr_auth_data():
+        return FormAuthData().ins().get_logged_in_session()
